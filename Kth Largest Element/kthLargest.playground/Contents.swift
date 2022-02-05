d73da3e25ba3ab59e526e4b9ed160f131c943e79
ian <38280903+iancarson@users.noreply.github.com>
2022-02-05 12:32:09 +0300

    Add files via upload

---
+++ Kth Largest Element/kthLargest.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+
+let a = [5, 1, 3, 2, 7, 6, 4]
+
+kthLargest(a, 0)
+kthLargest(a, 1)
+kthLargest(a, 2)
+kthLargest(a, 3)
+kthLargest(a, 4)
+kthLargest(a, 5)
+kthLargest(a, 6)
+kthLargest(a, 7)
+kthLargest(a, 8)
+
+randomizedSelect(a, order: 0)
+randomizedSelect(a, order: 1)
+randomizedSelect(a, order: 2)
+randomizedSelect(a, order: 3)
+randomizedSelect(a, order: 4)
+randomizedSelect(a, order: 5)
+randomizedSelect(a, order: 6)
